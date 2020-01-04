593c187530cc91c0618679e59045c22022ae791a
Gregory Henderson <gregoryhenderson@Gregorys-MacBook-Pro.local>
2020-01-04 14:24:48 -0600

    updating clicky game

---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Nav from "../Nav";
+import Header from "../Header";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from "../Footer";
+import data from "../../data.json";
+
+class Game extends Component{
+
+    
+
+
+    render(){
+        return(
+        <Container>
+            <Nav>
+
+
+            </Nav>
+        </Container>
+        );
+    }
+}
+
+export default Game;
